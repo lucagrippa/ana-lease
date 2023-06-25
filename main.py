b47716fc22404801aa5c97fa86250bcdd8cb00b1
lucagrippa <lucagrip@yahoo.com>
2023-06-25 14:40:26 -0400

    added file uploader and button

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import tempfile
 import streamlit as st
+from langchain.document_loaders import PyPDFLoader
 
 
 def main():
     st.title("📝 Lease Analyzer")
     uploaded_lease = st.file_uploader("Upload your lease!", type="pdf")
+
+    analyze_button = st.button("Analyze")
+
+    loader = PyPDFLoader("example_data/layout-parser-paper.pdf")
+    pages = loader.load_and_split()
 
 
 if __name__ == "__main__":
